02019b46f9905aa85aecdffa4d56b40fad3cd7db
Paul McCarthy <pauldmccarthy@gmail.com>
2017-10-17 12:30:32 +0200

    Fixed some typos. Added an info panel which contains attributes for the currently selected item.  A wx event is posted when a file is double-clicked.

--- wxnat/browser.py
+++ wxnat/browser.py
@@ -7,6 +7,7 @@
 
 import os.path as op
 import wx
+import wx.lib.newevent as wxevent
 
 import xnat
 
@@ -49,20 +50,29 @@ import fsleyes_widgets.widgetgrid           as wgrid
 
 
 XNAT_HIERARCHY = {
-    'projects'    : ['subjects',          'resources'],
-    'subjects'    : ['experiments',       'resources'],
-    'experiments' : ['assesors', 'scans', 'resources'],
+    'projects'    : ['subjects',           'resources'],
+    'subjects'    : ['experiments',        'resources'],
+    'experiments' : ['assessors', 'scans', 'resources'],
     'scans'       : ['resources'],
     'resources'   : ['files'],
 }
 
 XNAT_NAME_ATT = {
-    'project'    : 'name}',
+    'project'    : 'name',
     'subject'    : 'label',
     'experiment' : 'label',
     'scan'       : 'type',
     'resource'   : 'label',
     'file'       : 'id',
+}
+
+XNAT_INFO_ATTS = {
+    'project'    : ['id', 'name'],
+    'subject'    : ['id', 'label'],
+    'experiment' : ['id', 'label'],
+    'scan'       : ['id', 'type'],
+    'resource'   : ['id', 'label', 'file_size'],
+    'file'       : ['id', 'size'],
 }
 
 # Saved hosts
@@ -102,11 +112,20 @@ class XNATBrowserPanel(wx.Panel):
         self.__connect  = wx.Button(self)
         self.__status   = wx.StaticText(self)
         self.__project  = wx.Choice(self)
-        self.__browser  = wx.TreeCtrl(self,
+        self.__splitter = wx.SplitterWindow(self,
+                                            style=(wx.SP_LIVE_UPDATE |
+                                                   wx.SP_BORDER))
+        self.__info     = wgrid.WidgetGrid(self.__splitter)
+        self.__browser  = wx.TreeCtrl(self.__splitter,
                                       style=(wx.TR_MULTIPLE    |
                                              wx.TR_NO_LINES    |
                                              wx.TR_HAS_BUTTONS |
                                              wx.TR_TWIST_BUTTONS))
+
+        self.__splitter.SetMinimumPaneSize(50)
+        self.__splitter.SplitHorizontally(self.__info, self.__browser)
+        self.__splitter.SetSashPosition(50)
+        self.__splitter.SetSashGravity(0.2)
 
         imagedir = op.join(op.dirname(__file__), '..', 'assets')
 
@@ -180,14 +199,15 @@ class XNATBrowserPanel(wx.Panel):
         self.__mainSizer.Add((1, 10))
         self.__mainSizer.Add(self.__projectSizer, flag=wx.EXPAND)
         self.__mainSizer.Add((1, 10))
-        self.__mainSizer.Add(self.__browser, flag=wx.EXPAND, proportion=1)
+        self.__mainSizer.Add(self.__splitter, flag=wx.EXPAND, proportion=1)
         self.__mainSizer.Add((1, 10))
 
         self.SetSizer(self.__mainSizer)
 
-        self.__connect.Bind(wx.EVT_BUTTON, self.__onConnect)
-        self.__project.Bind(wx.EVT_CHOICE, self.__onProject)
-        self.__browser.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.__onTreeSelect)
+        self.__connect.Bind(wx.EVT_BUTTON,              self.__onConnect)
+        self.__project.Bind(wx.EVT_CHOICE,              self.__onProject)
+        self.__browser.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.__onTreeActivate)
+        self.__browser.Bind(wx.EVT_TREE_SEL_CHANGED,    self.__onTreeSelect)
 
         self.__session = None
         self.disconnect()
@@ -239,7 +259,7 @@ class XNATBrowserPanel(wx.Panel):
 
         if not (host.startswith('http://') or
                 host.startswith('https://')):
-            host = 'http://' + host
+            host = 'https://' + host
 
         if username == '': username = None
         if password == '': password = None
@@ -260,28 +280,45 @@ class XNATBrowserPanel(wx.Panel):
         project = self.__project.GetString(self.__project.GetSelection())
 
         self.__browser.DeleteAllItems()
-        self.__browser.AddRoot(
-            project,
-            data=['projects', self.__session.projects[project]])
+        root = self.__browser.AddRoot(
+            'project {}'.format(project),
+            data=[self.__session.projects[project], 'projects'],
+            image=self.__unloadedFolderImageId)
+        self.__onTreeSelect(item=root)
 
 
-    def __onTreeSelect(self, ev=None, item=None):
+    def __onTreeActivate(self, ev=None, item=None):
 
         if ev is not None:
             item = ev.GetItem()
             ev.Skip()
 
-        level, obj = self.__browser.GetItemData(item)
+        obj, level = self.__browser.GetItemData(item)
 
         if level == 'files':
-            print('File: {}'.format(self.__browser.GetItemText(item)))
-            print(' uri: {}'.format(obj.uri))
+
+            print('XNATFileSelectEvent: {}'.format(obj.uri))
+
+            ev = XNATFileSelectEvent(path=obj.uri)
+            ev.SetEventObject(self)
+            wx.PostEvent(self, ev)
             return
 
         print('Folder {}'.format(self.__browser.GetItemText(item)))
 
         if self.__browser.GetChildrenCount(item) > 0:
             return
+
+        # TODO For objects with a larger number of children:
+        #
+        #       - show a dialog, allow the user to cancel the
+        #         request
+        #         OR
+        #       - Only load up to e.g. 100 items, and have a
+        #         button  allowing the user to load more
+        #         OR
+        #       - Prompt the user to select a range of items
+        #         to load
 
         self.__browser.SetItemImage(item, self.__loadedFolderImageId)
 
@@ -300,9 +337,59 @@ class XNATBrowserPanel(wx.Panel):
 
                 self.__browser.AppendItem(
                     item,
-                    label,
+                    '{} {}'.format(catt[:-1], label),
                     image=image,
-                    data=[catt, child])
+                    data=[child, catt])
+
+
+    def __onTreeSelect(self, ev=None, item=None):
+
+        if ev is not None:
+            ev.Skip()
+            item = ev.GetItem()
+
+        obj, level = self.__browser.GetItemData(item)
+        otype      = level[:-1]
+
+        rows = [
+            ('Type', otype),
+        ]
+
+        for att in XNAT_INFO_ATTS[otype]:
+            print('Lookup {}.{} [{}]'.format(otype, att, obj))
+            try:
+                rows.append((att, str(getattr(obj, att))))
+            except Exception:
+                continue
+
+        if level in XNAT_HIERARCHY:
+            for catt in XNAT_HIERARCHY[level]:
+                nchildren = len(getattr(obj, catt))
+                rows.append((catt, str(nchildren)))
+
+        self.__info.ClearGrid()
+        self.__info.SetGridSize(len(rows), 2, growCols=(1, ))
+
+        for i, (header, value) in enumerate(rows):
+            self.__info.SetText(i, 0, header)
+            self.__info.SetText(i, 1, value)
+
+        self.__info.Refresh()
+
+
+_XNATFileSelectEvent, _EVT_XNAT_FILE_SELECT_EVENT = wxevent.NewEvent()
+
+
+EVT_XNAT_FILE_SELECT_EVENT = _EVT_XNAT_FILE_SELECT_EVENT
+"""Identifier for the :data:`XNATFileSelectEvent`. """
+
+
+XNATFileSelectEvent = _XNATFileSelectEvent
+"""Event emitted when a file item in the XNAT tree viewer is selected,
+either by it being double-clicked, or with the enter key pressed while
+it is highlighted. Contains an attribute ``path``, which may be passed
+to the :meth:`XNATBrowserPanel.download` method to download the file.
+"""
 
 
 class XNATBrowserDialog(wx.Dialog):
@@ -322,3 +409,12 @@ class XNATBrowserDialog(wx.Dialog):
 
         self.__sizer.Layout()
         self.__sizer.Fit(self)
+
+
+if __name__ == '__main__':
+    app   = wx.App()
+    # frame = wx.Frame(None)
+    dlg = XNATBrowserDialog(None)
+    dlg.SetSize((-1, 500))
+    dlg.Show()
+    app.MainLoop()
