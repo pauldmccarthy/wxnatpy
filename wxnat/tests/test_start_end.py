d897a193fda1e6c88d42be4ac1a66a843ef9315c
Paul McCarthy <pauldmccarthy@gmail.com>
2021-10-06 17:05:43 +0100

    TEST: Make tests work

--- wxnat/tests/test_start_end.py
+++ wxnat/tests/test_start_end.py
@@ -5,11 +5,14 @@
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
 
+import time
+
 import wx
 
-from . import run_with_wx
+from . import run_with_wx, yield_until
 
 from wxnat import XNATBrowserPanel
+
 
 def test_start_end():
     run_with_wx(_test_start_end)
@@ -17,11 +20,29 @@ def _test_start_end():
 
     parent = wx.GetTopLevelWindows()[0]
     panel  = XNATBrowserPanel(parent)
+    result = [None]
 
-    assert not panel.StartSession('not.a.xnat.server', showError=False)
+    def callback(val):
+        result[0] = val
+
+    panel.StartSession('not.a.xnat.server',
+                       showError=False,
+                       callback=callback)
+
+    yield_until(lambda : result[0] is not None)
+
+    assert not result[0]
     assert not panel.SessionActive()
 
-    assert panel.StartSession('central.xnat.org', showError=False)
+    result[0] = None
+
+    panel.StartSession('central.xnat.org',
+                       showError=False,
+                       callback=callback)
+
+    yield_until(lambda : result[0] is not None)
+
+    assert result[0]
     assert panel.SessionActive()
     panel.EndSession()
     assert not panel.SessionActive()
